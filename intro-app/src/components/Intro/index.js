064a9e935d77603497c60c48e88f34b18921ba89
SGrossett <selene_816@hotmail.com>
2022-04-28 15:07:58 -0400

    Style the intro content for the desktop view.

--- intro-app/src/components/Intro/index.js
+++ intro-app/src/components/Intro/index.js
@@ -16,30 +16,30 @@ function Intro() {
       <Container className='d-flex justify-content-center'>
 
         <div class="row content">
-          <div class="col-sm-6 p-0">            
+          <div class="col-md-6 p-0">            
             <div>
               <img className='w-100' src={mobileImg} />
             </div>
           </div>
 
-          <div class='col-sm-1 p-0'>
-            <div class="col-sm-5 content">
-              <h1 className='mt-5 headline'>Make remote work</h1>
-              <p className='mt-3'>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
-              <button className='mt-2 learn'>Learn more</button>
-              <div className='row container m-0 my-5'>
-                <div className='col-3 '>
-                  <img className='w-100' src={databiz} />
-                </div>
-                <div className='col-3'>
-                  <img className='w-100' src={audiophile} />
-                </div>
-                <div className='col-3'>
-                  <img className='w-100' src={meet} />
-                </div>
-                <div className='col-3'>
-                  <img className='w-100' src={maker} />
-                </div>
+          <div class='col-md-1 p-0'></div>
+
+          <div class="col-md-5 content">
+            <h1 className='mt-5 headline'>Make <span>remote work</span></h1>
+            <p className='mt-3'>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
+            <button className='mt-2 learn'>Learn more</button>
+            <div className='row container m-0 my-5'>
+              <div className='col-3 '>
+                <img className='w-100' src={databiz} />
+              </div>
+              <div className='col-3'>
+                <img className='w-100' src={audiophile} />
+              </div>
+              <div className='col-3'>
+                <img className='w-100' src={meet} />
+              </div>
+              <div className='col-3'>
+                <img className='w-100' src={maker} />
               </div>
             </div>
           </div>
