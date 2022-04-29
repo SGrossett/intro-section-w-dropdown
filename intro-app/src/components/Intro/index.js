4a8245922b121cf80b47e79497a8df0bc14fdf32
SGrossett <selene_816@hotmail.com>
2022-04-28 23:57:21 -0400

    Clean up into code and remove and variables, imports, or comments that are not being used.

--- intro-app/src/components/Intro/index.js
+++ intro-app/src/components/Intro/index.js
@@ -1,6 +1,6 @@
 import { useMediaQuery } from 'react-responsive';
 import { Container } from 'react-bootstrap';
-import Media from 'react-media';
+
 import './css/intro.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -11,12 +11,7 @@ import audiophile from '../../images/client-audiophile.svg';
 import meet from '../../images/client-meet.svg';
 import maker from '../../images/client-maker.svg';
 
-
-
 function Intro() {
-  const isDesktopOrLaptop = useMediaQuery({
-    query: '(min-width: 1224px)'
-  })
   const desktopView = useMediaQuery({ query: '(min-width: 768px)' })
   const mobileView = useMediaQuery({ query: '(max-width: 767px)' })
   return (
@@ -26,22 +21,8 @@ function Intro() {
         <div class="row reverse content">
           <div class="col-md-5 p-0">            
             <div className='image'>
-              {/* <picture className='image'>
-                <source 
-                  media="(min-width: 768px)" srcset={desktopImg} className='w-100'/>
-                <source 
-                  media="(max-width: 767px)" srcset={mobileImg} className='w-100'/>
-                <img 
-                  src="https://image.freepik.com/free-vector/web-development-design_23-2147508519.jpg?1" 
-                  alt="Flowers" 
-                  className='w-100' 
-                />
-              </picture> */}
-            {/* <div id="main-img" /> */}
-
-            {desktopView && <img className='w-100 h-100 img-responsive' src={desktopImg} />}
-            {mobileView && <img className='w-100' src={mobileImg} />}
-              {/* <img className='w-100' src={mobileImg} /> */}
+              {desktopView && <img className='w-100 h-100 img-responsive' src={desktopImg} />}
+              {mobileView && <img className='w-100' src={mobileImg} />}
             </div> 
           </div>
 
