ebaf8a102c66f9382c7140b1b77a0df44c8515af
SGrossett <selene_816@hotmail.com>
2022-04-28 21:49:37 -0400

    Update the markup and stying for the desktop image because it wasn't aligning correctly.

--- intro-app/src/components/Intro/index.js
+++ intro-app/src/components/Intro/index.js
@@ -1,25 +1,48 @@
+import { useMediaQuery } from 'react-responsive';
 import { Container } from 'react-bootstrap';
-
+import Media from 'react-media';
 import './css/intro.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import mobileImg from '../../images/image-hero-mobile.png';
-import deskTopMobile from '../../images/image-hero-desktop.png';
+import desktopImg from '../../images/image-hero-desktop.png';
 import databiz from '../../images/client-databiz.svg';
 import audiophile from '../../images/client-audiophile.svg';
 import meet from '../../images/client-meet.svg';
 import maker from '../../images/client-maker.svg';
 
+
+
 function Intro() {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-width: 1224px)'
+  })
+  const desktopView = useMediaQuery({ query: '(min-width: 768px)' })
+  const mobileView = useMediaQuery({ query: '(max-width: 767px)' })
   return (
-    <div class="">
+    <div class="mt-3">
       <Container className='d-flex justify-content-center'>
 
-        <div class="row content">
-          <div class="col-md-6 p-0">            
-            <div>
-              <img className='w-100' src={mobileImg} />
-            </div>
+        <div class="row reverse content">
+          <div class="col-md-5 p-0">            
+            <div className='image'>
+              {/* <picture className='image'>
+                <source 
+                  media="(min-width: 768px)" srcset={desktopImg} className='w-100'/>
+                <source 
+                  media="(max-width: 767px)" srcset={mobileImg} className='w-100'/>
+                <img 
+                  src="https://image.freepik.com/free-vector/web-development-design_23-2147508519.jpg?1" 
+                  alt="Flowers" 
+                  className='w-100' 
+                />
+              </picture> */}
+            {/* <div id="main-img" /> */}
+
+            {desktopView && <img className='w-100 h-100 img-responsive' src={desktopImg} />}
+            {mobileView && <img className='w-100' src={mobileImg} />}
+              {/* <img className='w-100' src={mobileImg} /> */}
+            </div> 
           </div>
 
           <div class='col-md-1 p-0'></div>
@@ -28,7 +51,7 @@ function Intro() {
             <h1 className='mt-5 headline'>Make <span>remote work</span></h1>
             <p className='mt-3'>Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.</p>
             <button className='mt-2 learn'>Learn more</button>
-            <div className='row container m-0 my-5'>
+            <div className='row container m-0 mt-5'>
               <div className='col-3 '>
                 <img className='w-100' src={databiz} />
               </div>
